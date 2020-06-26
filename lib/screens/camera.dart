90db4c64e8af8b9229b6588f723960c1a2219cde
rlaqudwls25 <rlaqudwls25@naver.com>
2020-06-26 16:27:15 +0900

    카메라 기능 구현

--- lib/screens/camera.dart
+++ lib/screens/camera.dart
@@ -1,64 +1,118 @@
+import 'dart:async';
+import 'dart:io';
+import 'package:camera/camera.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_grid_button/flutter_grid_button.dart';
 import 'package:wondersappver02/main.dart';
 
 
-class Camera extends StatelessWidget {
+class Camera extends StatefulWidget {
+  const Camera({Key key}) : super(key: key);
+
+  @override
+  CameraState createState() => CameraState();
+}
+
+class CameraState extends State<Camera> {
+
+  CameraController _controller;
+  List<CameraDescription> _cameras;
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  bool _isRecordingMode = false;
+  bool _isRecording = false;
+
+  @override
+  void initState() {
+    _initCamera();
+    super.initState();
+  }
+
+  Future<void> _initCamera() async {
+    _cameras = await availableCameras();
+    _controller = CameraController(_cameras[0], ResolutionPreset.medium);
+    _controller.initialize().then((_) {
+      if (!mounted) {
+        return;
+      }
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller?.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return MaterialApp(
-        theme: ThemeData(primarySwatch: Colors.red),
-        home: Scaffold(
-          appBar: AppBar(
-            centerTitle: true,
-            title: Text('카메라',style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold),),
-            leading: IconButton(
-              icon: Icon(Icons.arrow_back),
-              onPressed: () {
-                Navigator.pop(context);
-              },
-            ),
-          ),
-          body: Column(
-            children: <Widget>[
-              Container(
-                margin: EdgeInsets.all(50),
-                child: Image.asset(
-                  'images/camera.png',
-                  width: 400.0,
-                  height: 250.0,
-                  fit: BoxFit.contain,
+    if (_controller != null) {
+      if (!_controller.value.isInitialized) {
+        return Container();
+      }
+    } else {
+      return const Center(
+        child: SizedBox(
+          width: 10,
+          height: 10,
+          child: CircularProgressIndicator(),
+        ),
+      );
+    }
+    if (!_controller.value.isInitialized) {
+      return Container();
+    }
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Camera', style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black),),
+      ),
+      backgroundColor: Theme.of(context).backgroundColor,
+      key: _scaffoldKey,
+      extendBody: true,
+      body: Column(
+        children: <Widget>[
+          _buildCameraPreview(),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                RaisedButton(
+                  color: Colors.black,
+                  child: Text("예", style: TextStyle(color: Colors.white, fontSize:  16),),
+                  onPressed: () {
+                  },
                 ),
-              ),
-              new Padding(padding: EdgeInsets.only(top: 10.0)),
-              Text('이미지가 선명합니까?',
-                  style:
-                  TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0)),
-              SizedBox(height: 50),
-
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-
-                children: <Widget>[
-                  FlatButton(
-                    child: Text('예',style: TextStyle(fontSize: 16),
-                    ),
-                    onPressed: (){},
-                    color: Colors.black,
-                    textColor: Colors.white,
-                  ),
-                  FlatButton(
-                    child: Text('아니오',style: TextStyle(fontSize: 16),),
-                    onPressed: (){},
-                    color: Colors.black,
-                    textColor: Colors.white,
-                  )
-                ],
+              ],
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              RaisedButton(
+                  color : Colors.black,
+                  child: Text("아니오", style: TextStyle(color: Colors.white, fontSize:  16),),
+                  onPressed: (){
+                  }
               )
             ],
-
-          ),
-        ));
+          )
+        ],
+      ),
+    );
   }
-}
+  Widget _buildCameraPreview() {
+    final size = MediaQuery.of(context).size;
+    return ClipRect(
+      child: Container(
+        child: Transform.scale(
+          scale: _controller.value.aspectRatio / size.aspectRatio,
+          child: Center(
+            child: AspectRatio(
+              aspectRatio: _controller.value.aspectRatio,
+              child: CameraPreview(_controller),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+  }
