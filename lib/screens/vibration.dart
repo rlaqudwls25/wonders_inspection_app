f0f37448cf581ee6ad9e13472170ba4b35d4d872
kdh8909 <kdh8909@gmail.com>
2020-06-26 17:15:27 +0900

    진동 기능 UI 수정

--- lib/screens/vibration.dart
+++ lib/screens/vibration.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:vibration/vibration.dart' as Vibrator;
 
-class Vibration extends StatefulWidget{
+class Vibration extends StatefulWidget {
   @override
   _VibrationState createState() => _VibrationState();
 }
 
 class _VibrationState extends State<Vibration> {
-
   bool _hasVibrator = false;
 
   @override
@@ -31,8 +30,10 @@ class _VibrationState extends State<Vibration> {
       appBar: AppBar(
         backgroundColor: Colors.red,
         centerTitle: true,
-        title: Text('진동검사',
-          style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),),
+        title: Text(
+          '진동검사',
+          style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+        ),
         leading: IconButton(
           icon: Icon(Icons.arrow_back),
           onPressed: () {
@@ -40,28 +41,41 @@ class _VibrationState extends State<Vibration> {
           },
         ),
       ),
-      backgroundColor: Colors.black,
+      backgroundColor: Colors.white,
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          Container(
-            margin: EdgeInsets.all(20),
-            child: Image.asset(
-              'images/flash.png',
-              width: 400.0,
-              height: 250.0,
-              fit: BoxFit.contain,
+          GestureDetector(
+            onTap: () {
+              Vibrator.Vibration.vibrate(duration: 1000);
+            },
+            child: Center(
+              child: Container(
+                height: 100,
+                width: 100,
+                margin: EdgeInsets.all(20),
+                child: Icon(Icons.vibration, size: 50, color: Colors.white),
+                decoration: BoxDecoration(
+                    color: Colors.red,
+                    shape: BoxShape.circle,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey[500],
+                        offset: Offset(4.0, 4.0),
+                        blurRadius: 10.0,
+                        spreadRadius: 1.0,
+                      ),
+                    ]),
+              ),
             ),
           ),
           Padding(padding: EdgeInsets.only(top: 50.0)),
-          Text(_hasVibrator ? '' : '진동 기능이 없는 기종입니다.'),
-          RaisedButton(
-            child: Text('진동'),
-            onPressed: () {
-              Vibrator.Vibration.vibrate(duration: 1000);
-            } ,
+          Text(
+            _hasVibrator ? '진동 버튼을 눌러보세요.' : '진동 기능이 없는 기종입니다.',
+            style: TextStyle(fontSize: 18),
           ),
         ],
       ),
-    ) ;
+    );
   }
 }
