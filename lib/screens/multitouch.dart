748105cc428f9fcff02023d045c4b43ffb20a1fd
kdh8909 <kdh8909@gmail.com>
2020-06-29 18:36:44 +0900

    다중 터치 구현

--- lib/screens/multitouch.dart
+++ lib/screens/multitouch.dart
@@ -1,53 +1,119 @@
+import 'dart:math';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class Multitouch extends StatelessWidget {
+class Multitouch extends StatefulWidget {
+  @override
+  _MultitouchState createState() => _MultitouchState();
+}
+
+class _MultitouchState extends State<Multitouch> {
+  bool firstCircleClicked = false;
+  bool secondCircleClicked = false;
+
+  double firstCircleTop = Random().nextInt(200).toDouble() + 50;
+  double firstCircleLeft = Random().nextInt(50).toDouble() + 50;
+  double firstCircleWidth = Random().nextInt(50).toDouble() + 50;
+  double firstCircleHeight = Random().nextInt(50).toDouble() + 50;
+
+  double secondCircleTop = Random().nextInt(200).toDouble() + 50;
+  double secondCircleRight = Random().nextInt(50).toDouble() + 50;
+  double secondCircleWidth = Random().nextInt(50).toDouble() + 70;
+  double secondCircleHeight = Random().nextInt(50).toDouble() + 70;
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return MaterialApp(
       theme: ThemeData(primarySwatch: Colors.red),
       home: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text(
-            '다중 터치',
-            style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+          appBar: AppBar(
+            centerTitle: true,
+            title: Text(
+              '다중 터치',
+              style:
+                  TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+            ),
+            leading: IconButton(
+              icon: Icon(Icons.arrow_back),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
           ),
-          leading: IconButton(
-            icon: Icon(Icons.arrow_back),
-            onPressed: () {
-              Navigator.pop(context);
-            },
-          ),
-        ),
-        body: Container(
-          padding: EdgeInsets.all(50.0),
-          child: Column(
+          body: Stack(
+            alignment: Alignment.center,
             children: <Widget>[
-              Padding(padding: EdgeInsets.all(30.0)),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget>[
-                  touch(''),
-                ],
+              Positioned(
+                top: firstCircleTop,
+                left: firstCircleLeft,
+                child: GestureDetector(
+                  onTapDown: (TapDownDetails td) {
+                    setState(() {
+                      firstCircleClicked = true;
+                    });
+                  },
+                  onTapUp: (TapUpDetails tu) {
+                    setState(() {
+                      firstCircleClicked = false;
+                    });
+                  },
+                  onTapCancel: (){
+                    setState(() {
+                      firstCircleClicked = false;
+                    });
+                  },
+                  child: Container(
+                    width: firstCircleWidth,
+                    height: firstCircleHeight,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(color: Colors.red, width: 1.0),
+                      color: firstCircleClicked ? Colors.blue : Colors.white,
+                    ),
+
+                  ),
+                ),
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: <Widget>[
-                  touch(''),
-                ],
+              Positioned(
+                top: secondCircleTop,
+                right: secondCircleRight,
+                child: GestureDetector(
+                  onTapDown: (TapDownDetails td){
+                    setState(() {
+                      secondCircleClicked = true;
+                    });
+                  },
+                  onTapUp: (TapUpDetails tu){
+                    setState(() {
+                      secondCircleClicked = false;
+                    });
+                  },
+                  onTapCancel: (){
+                    setState(() {
+                      secondCircleClicked = false;
+                    });
+                  },
+                  child: Container(
+                    width: secondCircleWidth,
+                    height: secondCircleHeight,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(color: Colors.red, width: 1.0),
+                      color: secondCircleClicked ? Colors.blue : Colors.white,
+                    ),
+                  ),
+                ),
               ),
-              new Padding(padding: EdgeInsets.only(top: 15.0)),
-              Text(
-                '손가락을 이용하여 화면 상의 '
-                    '두 원을 동시에 눌러주세요',
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25.0),
+              Positioned(
+                bottom: 50,
+                child: Text(
+                  '손가락을 이용하여 화면 상의 '
+                  '두 원을 동시에 눌러주세요',
+                  style: TextStyle(fontSize: 17.0),
+                ),
               ),
             ],
-          ),
-        ),
-      ),
+          )),
     );
   }
 
