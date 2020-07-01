536606d5ff0d8fc51cf1925790246c7c567cdc56
rlaqudwls25 <rlaqudwls25@naver.com>
2020-07-01 15:52:38 +0900

    touch 기능 구현 수정(2)

--- lib/screens/touch.dart
+++ lib/screens/touch.dart
@@ -1,12 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home: Touch(),
-  ));
-}
-
 class Touch extends StatefulWidget {
   @override
   _TouchState createState() => new _TouchState();
